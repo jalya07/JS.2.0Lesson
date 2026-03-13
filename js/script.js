f485c9bb23be31df2b25384bbb564b64e8ec2567
jalya07 <jalavh@code.edu.az>
2026-03-13 15:38:39 +0400

    change

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
-let buttons=document.querySelectorAll(".btn-primary");
+let buttons = document.querySelectorAll(".btn-primary");
 for (let i = 0; i < buttons.length; i++) {
     const button = buttons[i];
-    button.addEventListener("click",function(ev){
+    button.addEventListener("click", function (ev) {
         ev.preventDefault();
-        console.log(this.parentNode.firstElementChild.innerText);
-        
+        let str = this.previousElementSibling.previousElementSibling.innerText
+        let id = this.parentNode.getAttribute("data-id");
+        let basketStr = localStorage.getItem("basket");
+        let products = [];
+        if (basketStr) {
+            products = JSON.parse(basketStr);
+        }
+        let existproduct = products.find(p => p.id == id);
+        if (existproduct) {
+            existproduct.count++;
+        }
+        else {
+            let product = {
+                id,
+                name: this.parentNode.firstElementChild.innerText,
+                desc: this.previousElementSibling.innerText,
+                price: str.substring(str.indexOf(":") + 1),
+                url: this.parentNode.previousElementSibling.getAttribute("src"),
+                count:1
+            }
+            products.push(product);
+        }
+
+        localStorage.setItem("basket", JSON.stringify(products))
+        // console.log(product);
+
     })
-    
+
 }
